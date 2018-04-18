cd5b9394f23171a7643334404922e1b5d350f9e6
Anup <anup12596@gmail.com>
2018-04-18 19:12:36 -0400

    Added user dashboard directories

---
+++ web/src/components/projectlist.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <md-card md-with-hover>
+        <div class="md-title"> Projects </div>
+    </md-card>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+export default {
+  name: 'Normal'
+}
+</script>
--- web/src/main.js
+++ web/src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(VueMaterial);
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
