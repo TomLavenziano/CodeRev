d60352a4052b09dceeb11ead170762f113013083
Anup <anup12596@gmail.com>
2018-04-25 15:41:01 -0400

    Created various components that will make up the dashboard view as well as the dashboard view itself

--- web/src/main.js
+++ web/src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import VueMaterial from 'vue-material';
-import 'vue-material/dist/vue-material.css';
+import 'vuetify/dist/vuetify.min.css';
+import UserDash from './components/UserDash.vue';
+import NavPanel from './components/NavigationPanel.vue';
+import Notifications from './components/Notifications.vue';
 
+Vue.use(Vuetify);
+Vue.component('my-userdash', UserDash);
+Vue.component('my-navpanel', NavPanel);
+Vue.component('my-notifs', Notifications);
 Vue.config.productionTip = false;
-Vue.use(VueMaterial);
+
 new Vue({
     router,
     store,
