727c4f9ff755db1f3309fd3479531cbad92f7d3f
Anup <anup12596@gmail.com>
2018-04-25 18:20:40 -0400

    add components

---
+++ web/src/components/Notifications.vue
@@ -0,0 +1,15 @@
+<template>
+<v-layout>
+
+    <v-card>
+
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">Notifications</h3>
+        </div>
+      </v-card-title>
+
+    </v-card>
+
+</v-layout>
+</template>
---
+++ web/src/views/UserDash.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <my-navpanel></my-navpanel>
+    <my-notifs></my-notifs>
+  </v-app>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
