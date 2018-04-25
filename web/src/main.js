532669e750f6335093ed6aa616233398ab57c445
Tom Lavenziano <tomlavenziano@epochzero.com>
2018-04-25 19:32:30 -0400

    Added Vuetify

--- web/src/main.js
+++ web/src/main.js
@@ -1,18 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
-import Vuetify from 'vuetify'
-import('../node_modules/vuetify/dist/vuetify.min.css')
-Vue.use(Vuetify)
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import Vuetify from 'vuetify';
+import '../node_modules/vuetify/dist/vuetify.min.css';
+Vue.use(Vuetify);
 
-
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
