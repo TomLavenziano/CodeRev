400b2befe4983e5ff2254a3509e9e4410a984063
Tom Lavenziano <tomlavenziano@epochzero.com>
2018-04-27 18:36:43 -0400

    Removed dependencies from main.js

--- web/src/main.js
+++ web/src/main.js
@@ -7,20 +7,20 @@ import Vuetify from 'vuetify';
 import '../node_modules/vuetify/dist/vuetify.min.css';
 import './registerServiceWorker';
 
-import UserDash from './views/UserDash.vue';
-import NavPanel from './components/NavigationPanel.vue';
-import Notifications from './components/Notifications.vue';
-import ToolBar from './components/ToolBar.vue';
-import dLay from '/.components/DashboardLayout.vue';
+// import UserDash from './views/UserDash.vue';
+// import NavPanel from './components/NavigationPanel.vue';
+// import Notifications from './components/Notifications.vue';
+// import ToolBar from './components/ToolBar.vue';
+// import dLay from '/.components/DashboardLayout.vue';
 
 Vue.use(VueSession, { persist: true });
 Vue.use(Vuetify);
 
-Vue.component('d-lay', dLay);
-Vue.component('my-userdash', UserDash);
-Vue.component('my-navpanel', NavPanel);
-Vue.component('my-notifs', Notifications);
-Vue.component('my-tb', ToolBar
+// Vue.component('d-lay', dLay);
+// Vue.component('my-userdash', UserDash);
+// Vue.component('my-navpanel', NavPanel);
+// Vue.component('my-notifs', Notifications);
+// Vue.component('my-tb', ToolBar);
 Vue.config.productionTip = false;
 
 new Vue({
