50e2a032c964d3961d8476c49844d5231e4bc639
Anup <anup12596@gmail.com>
2018-05-02 17:37:14 -0400

    Built Project List component further

--- web/src/components/ProjectList.vue
+++ web/src/components/ProjectList.vue
@@ -8,11 +8,11 @@
     <template slot="items" slot-scope="props">
       <tr @click="props.expanded = !props.expanded">
         <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
+        <td class="text-xs-center">{{ props.item.notifications }}</td>
+        <td class="text-xs-center">{{ props.item.commits }}</td>
+        <td class="text-xs-center">{{ props.item.reviews}}</td>
+        <td class="text-xs-center">{{ props.item.todos }}</td>
+        <td class="text-xs-center">{{ props.item.iron }}</td>
       </tr>
     </template>
     <template slot="expand" slot-scope="props">
@@ -36,11 +36,19 @@
           },
           { text: 'Notifications', value: 'notifications', sortable: false },
           { text: 'Commits', value: 'commits', sortable: false },
-          { text: 'Pending Reviews', value: 'previews', sortable: false },
-          { text: 'ToDos', value: 'protein', sortable: false },
+          { text: 'Pending Reviews', value: 'reviews', sortable: false },
+          { text: 'ToDos', value: 'todos' },
           
         ],
         items: [
+          {
+            name: "Sample Project",
+            notifications: 25,
+            commits: 4,
+            reviews: 10,
+            todos: 1
+
+          }
         ]
       }
     }
