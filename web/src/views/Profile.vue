57995155a0f625c43d9ea68934999e4e65918ecd
Tom Lavenziano <tomlavenziano@epochzero.com>
2018-05-04 19:09:10 -0400

    Merge conflict

--- web/src/views/Profile.vue
+++ web/src/views/Profile.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
 <div class="profile">
     <v-container fluid grid-list-sm>
         <v-layout row wrap>
@@ -32,41 +31,6 @@
                 <v-card>
                     <v-list two-line style="padding: 0px">
                         <template>
-=======
-    <div class="profile">
-        <v-container fluid grid-list-sm>
-            <v-layout row wrap>
-                <v-flex xs24 sm12>
-                    <v-card>
-                        <v-card-media v-bind:src="require('@/assets/user-profile-bg.jpg')" height="200px">
-                        </v-card-media>
-                        <v-card-title primary-title>
-                            <div>
-                                <v-avatar>
-                                    <a href="https://github.com/akhilapamukuntla" target="_blank" style="color: white"><img v-bind:src="require('@/assets/mypic.jpg')" alt="John" style="width:100px; height:100px; margin-top:-70px;margin-left: 100px"></a>
-                                </v-avatar>
-                            </div>
-                        </v-card-title>
-                    </v-card>
-                </v-flex>
-            </v-layout>
-            <v-layout row wrap style="margin-top: 40px;">
-                <v-flex xs6 sm3>
-                    <v-card color="blue">
-                        <v-card-title primary-title>
-                            <div>
-                                <div style="color:white;font-size: 30px;">{{user.login}}</div>
-                                <p style="color:white;font-size:20px">{{user.bio}}</p>
-
-                            </div>
-                        </v-card-title>
-                    </v-card>
-                </v-flex>
-                <v-flex xs6 sm3 offset-sm1>
-                    <v-card>
-                        <v-list two-line style="padding: 0px">
-                            <template>
->>>>>>> profile
                                 <v-list-tile style="background: #f6f8fa;">
                                     <v-list-tile-content>
                                         <v-list-tile-title><b style="font-size: 20px">Associated Projects</b></v-list-tile-title>
@@ -146,9 +110,5 @@ export default {
         console.log('hii');
         this.getUsers();
     }
-<<<<<<< HEAD
 }
-=======
-};
->>>>>>> profile
 </script>
