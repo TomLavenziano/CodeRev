9627e90ec57795c0aec17c47158d7e61150e4934
Tom Lavenziano <tomlavenziano@epochzero.com>
2018-05-04 17:12:50 -0400

    Config changes

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  lintOnSave: false
-}
+    lintOnSave: false,
+
+    devServer: {
+        open: process.platform === 'linux',
+        host: '0.0.0.0',
+        port: 8080,
+        https: true,
+        hotOnly: false,
+        // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
+        // Proxy is used to point all reqs that don't match a route to another place, like an API
+        proxy: null, // string | Object
+        before: app => {}
+    }
+};
