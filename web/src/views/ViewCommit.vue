b480c5e788ad2b3a7415fa0f11baaa8dc9b1cf26
Tom Lavenziano <tomlavenziano@epochzero.com>
2018-05-11 15:51:25 -0400

    Review misc new files

---
+++ web/src/views/ViewCommit.vue
@@ -0,0 +1,28 @@
+<!--
+    Display details and list reviews for an individual commit and allow you to start a new review
+-->
+
+<template>
+  <div id="ViewCommit">
+      <h4>Commit: {{ id }}</h4>
+      <ViewDiff pid="id"></ViewDiff>
+  </div>
+</template>
+<script>
+import ViewDiff from '@/components/ViewDiff.vue';
+
+export default {
+    name: 'ViewCommit',
+    components: {
+        ViewDiff
+    },
+    props: ['id'],
+    data() {
+        return {
+
+        };
+    }
+};
+</script>
+<style lang="less" scoped>
+</style>
